8a1d0a04f548619ac63ac5d7acf326ad1f676afe
limitles1975 <yasincalik1975@gmail.com>
2025-01-01 00:06:51 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Merhaba Limitless topluluğu! Bu bir başlangıç.")
+print("Merhaba Limitless topluluğu! Bu bir başlangıç.")
+print("Dark Bebek! Bu gece karanlık, çünkü AfterDark zamanı!")
+name = input("Adın ne, karanlık dostu? ")
+print(f"Selam {name}, bu gece AfterDark zamanı! Hadi karanlıkta ışık olalım!")
+
+
